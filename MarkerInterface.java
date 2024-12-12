1e216ae22c36c14afd52659e45328bd028a96a1d
Rishitha <trishithapandu2218@gmail.com>
2024-12-12 10:40:45 +0530

    Add files via upload

---
+++ MarkerInterface.java
@@ -0,0 +1,21 @@
+import java.util.*;
+interface MarkerInterface
+{
+}
+class h implements MarkerInterface
+{
+    public void print()
+    {
+        System.out.println("HELLO MOUNIKA GARU");
+    }
+}
+        
+class Main
+{
+    public static void main(String args[])
+    {
+        h r=new h();
+        r.print();
+    }
+
+}
