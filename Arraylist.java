94fd71b5d2832224aed6d9e41d950a8604982192
Rishitha <trishithapandu2218@gmail.com>
2024-12-12 21:21:53 +0530

    Add files via upload

---
+++ Arraylist.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Arraylist{
+    public static void main(String args[]){
+        ArrayList< Integer >a=new ArrayList< >();
+        a.add(22);
+        a.add(18);
+        System.out.println(a);
+    }
+}
